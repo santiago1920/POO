067eb8fcfd47dbacfdd3dda6382f9beb51204281
santiago1920 <142598928+santiago1920@users.noreply.github.com>
2023-09-01 09:01:32 -0500

    Add files via upload

---
+++ ex_class_v/src/main/main.java
@@ -0,0 +1,12 @@
+package main;
+import poo.unicauca.edu.co.e1;
+public class main {
+    public static void main(String[] args) {
+        //Create instance
+        e1 call = new e1();
+        
+        System.out.println("First Name: " + call.firstName);
+        System.out.println("Last Name: " + call.lastName);
+    }
+    
+}
---
+++ ex_class_v/src/poo/unicauca/edu/co/e1.java
@@ -0,0 +1,5 @@
+package poo.unicauca.edu.co;
+public class e1 {
+    public String firstName = "Santiago";
+    public String lastName= "Rodriguez";
+}
