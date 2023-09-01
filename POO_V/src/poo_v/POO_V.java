bb5d333c24b9eaf912cc3a31c461953c237aaa39
santiago1920 <142598928+santiago1920@users.noreply.github.com>
2023-09-01 09:01:13 -0500

    Add files via upload

---
+++ POO_V/src/poo_v/POO_V.java
@@ -0,0 +1,12 @@
+package poo_v;
+public class POO_V {
+    public static void main(String[] args) {
+        
+        int document = 1061;
+        int  year_hbd = 2004;
+        String name = "Santiago";
+        
+        System.out.println("n");
+        
+    } 
+}
